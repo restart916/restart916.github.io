b4f58ffdf782155d9df9cac91c021d718b826288
Yoon Hyunjin <christine.hyunjin.yoon@gmail.com>
2019-04-18 13:02:04 +0200

    install VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Hyunjin yoon',
+  description: 'Hyunjin yoon',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { text: 'Contact', link: '/contact/' }
+    ],
+    // sidebar: [
+    //   ['/', 'Home'],
+    //   ['/about/', 'About'],
+    //   ['/contact/', 'Contact']
+    // ]
+  }
+}
