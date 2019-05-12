8d6a1bc0401901b7501ef881dd31f77da9f22db8
Yoon Hyunjin <christine.hyunjin.yoon@gmail.com>
2019-05-12 20:12:34 +0200

    use fontawesome

--- src/docs/.vuepress/config.js
+++ src/docs/.vuepress/config.js
@@ -5,6 +5,14 @@ const webpack = require("webpack");
 module.exports = {
   title: 'Hyunjin Yoon',
   description: 'blog',
+  head: [
+      ['link', {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.8.2/css/all.css',
+        integrity: 'sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay',
+        crossorigin: 'anonymous'
+      }]
+  ],
   themeConfig: {
     nav: [
       { text: 'Blog', link: '/' },
--- src/docs/.vuepress/theme/About.vue
+++ src/docs/.vuepress/theme/About.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="about py-md-5 px-0 py-4">
       <div>
-          About Hyunjin Yoon
+          <h2 class="mb-4">
+              About Hyunjin Yoon
+          </h2>
+          <div>
+              <div class="text-secondary">
+                  Front-End Developer or Product Designer<br>
+                  5년동안 프로덕트 디자이너로 일했고 Front-End 공부를 하고 있는 윤현진입니다.              
+              </div>
+              <div>
+                <a href="">
+                    email
+                </a>
+                <a href="http://tiny.cc/gxwp4y" target="_blank">
+                    디자인 포트폴리오
+                </a>
+                <a href="" target="_blank">
+                    <i class="fas fa-camera"></i>
+                </a>
+                <a href="" target="_blank">
+                    github
+                </a>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-6">
+
+              </div>
+              <div class="col-md-6">
+
+              </div>              
+          </div>
       </div>
   </div>
 </template>
